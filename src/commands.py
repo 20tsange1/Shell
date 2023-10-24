95ce69ec5ae3186d537375dc88165c00661ecfbc
Robbie Morris <77032126+robbiebmorris@users.noreply.github.com>
2023-10-24 15:38:24 +0100

    [REFAC]: Abstract shell.py into commands.py (#1)
    * [REFAC]: Abstract shell.py into commands.py
    
    * [REFAC]: Abstract shell.py

---
+++ src/commands.py
@@ -0,0 +1,104 @@
+import re
+import sys
+import os
+from os import listdir
+from collections import deque
+from glob import glob
+
+class Command:
+    def execute(self, args, out):
+        pass
+
+
+class PwdCommand(Command):
+    def execute(self, args, out):
+        out.append(os.getcwd())
+
+
+class CdCommand(Command):
+    def execute(self, args, out):
+        if len(args) == 0 or len(args) > 1:
+            raise ValueError("Wrong number of command line arguments")
+        os.chdir(args[0])
+
+
+class EchoCommand(Command):
+    def execute(self, args, out):
+        out.append(" ".join(args) + "\n")
+
+
+class LsCommand(Command):
+    def execute(self, args, out):
+        if len(args) == 0:
+            ls_dir = os.getcwd()
+        elif len(args) > 1:
+            raise ValueError("Wrong number of command line arguments")
+        else:
+            ls_dir = args[0]
+        for f in listdir(ls_dir):
+            if not f.startswith("."):
+                out.append(f + "\n")
+
+
+class CatCommand(Command):
+    def execute(self, args, out):
+        for a in args:
+            with open(a) as f:
+                out.append(f.read())
+
+
+class HeadCommand(Command):
+    def execute(self, args, out):
+        if len(args) != 1 and len(args) != 3:
+            raise ValueError("Wrong number of command line arguments")
+        if len(args) == 1:
+            num_lines = 10
+            file = args[0]
+        if len(args) == 3:
+            if args[0] != "-n":
+                raise ValueError("Wrong flags")
+            else:
+                num_lines = int(args[1])
+                file = args[2]
+        with open(file) as f:
+            lines = f.readlines()
+            for i in range(0, min(len(lines), num_lines)):
+                out.append(lines[i])
+
+
+class TailCommand(Command):
+    def execute(self, args, out):
+        if len(args) != 1 and len(args) != 3:
+            raise ValueError("Wrong number of command line arguments")
+        if len(args) == 1:
+            num_lines = 10
+            file = args[0]
+        if len(args) == 3:
+            if args[0] != "-n":
+                raise ValueError("Wrong flags")
+            else:
+                num_lines = int(args[1])
+                file = args[2]
+        with open(file) as f:
+            lines = f.readlines()
+            display_length = min(len(lines), num_lines)
+            for i in range(0, display_length):
+                out.append(lines[len(lines) - display_length + i])
+
+
+class GrepCommand(Command):
+    def execute(self, args, out):
+        if len(args) < 2:
+            raise ValueError("Wrong number of command line arguments")
+        pattern = args[0]
+        files = args[1:]
+        for file in files:
+            with open(file) as f:
+                lines = f.readlines()
+                for line in lines:
+                    if re.match(pattern, line):
+                        if len(files) > 1:
+                            out.append(f"{file}:{line}")
+                        else:
+                            out.append(line)
+
